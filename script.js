b13c1e4ead59c58db196ba4c0e9bca43b2f749e5
nikhilvarmab12 <nikhilvarmab12@gmail.com>
2026-05-25 18:19:59 +0530

    modified

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 
-// MEMORY CARD GAME
+ 
 
 console.log("Memory Card Game Started");
 
-// DOM ELEMENTS
+ 
 
-// Game board
+ 
 const gameBoard = document.getElementById("gameBoard");
 
-// Game info
+ 
 const movesDisplay = document.getElementById("moves");
 const timerDisplay = document.getElementById("time");
 
-// Buttons
+ 
 const restartButton = document.getElementById("restart");
 const pauseButton = document.getElementById("pauseButton");
 const startButton = document.getElementById("startButton");
 const exitButton = document.getElementById("exitButton");
 
-// Start screen
+ 
 const startScreen = document.getElementById("startScreen");
-// Player input
+ 
 const playerNameInput = document.getElementById("playerName");
 
-// Leaderboard elements
+ 
 const bestPlayerDisplay = document.getElementById("bestPlayer");
 const bestMovesDisplay = document.getElementById("bestMoves");
 const bestTimeDisplay = document.getElementById("bestTime");
@@ -33,29 +33,29 @@ const nextLevelButton =  document.getElementById("nextLevelButton");
 
 const closeVictoryButton = document.getElementById("closeVictoryButton");
 
-// Score
+ 
 const scoreDisplay = document.getElementById("score");
 
-// Achievements
+ 
 const achievementList = document.getElementById("achievementList");
 
 
-// Background music
+ 
 const bgMusic = new Audio("assets/sounds/bg-music.mp3");
 
-// Flip sound
+ 
 const flipSound = new Audio("assets/sounds/flip.mp3");
 
-// Match sound
+ 
 const matchSound = new Audio("assets/sounds/match.mp3");
 
-// Win sound
+ 
 const winSound = new Audio("assets/sounds/win.mp3");
 
 
 const difficultySelect = document.getElementById("difficulty");
 
-// Victory screen
+ 
 const victoryScreen =
     document.getElementById("victoryScreen");
 
@@ -65,7 +65,7 @@ const finalStats =
 const playAgainButton =
     document.getElementById("playAgainButton");
 
-// Confetti container
+ 
 const confettiContainer =
     document.getElementById("confettiContainer");
 
@@ -91,45 +91,45 @@ const allCards = [
     "🦄"
 ];
 
-// Active game cards
+ 
 let gameCards = [];
 
-// Store selected cards
+ 
 let flippedCards = [];
 
-// Prevent spam clicking
+ 
 let lockBoard = false;
 
-// Pause state
+ 
 let gamePaused = false;
 
-// Moves counter
+ 
 let moves = 0;
 
-// Timer
+ 
 let timer = 0;
 let timerInterval;
 
-// Matched cards counter
+ 
 let matchedCards = 0;
 
-// Current player
+ 
 let currentPlayer = "";
-// Current difficulty
+ 
 let currentDifficulty = "";
 
-// Score system
+ 
 let score = 0;
 
-// Combo multiplier
+ 
 let combo = 1;
 
-// Sound states
+ 
 let musicMuted = false;
 
 let soundMuted = false;
 
-// Achievements
+ 
 let achievements = [];
 
 
@@ -140,7 +140,7 @@ function setDifficulty() {
         gameCards = [...allCards.slice(0,4),
                      ...allCards.slice(0,4)];
 
-        // Update grid
+ 
         gameBoard.style.gridTemplateColumns =
             "repeat(4, 100px)";
     }
@@ -159,12 +159,12 @@ function setDifficulty() {
     else{
 
 
-        gameCards = [...allCards.slice(0,12),
-                     ...allCards.slice(0,12)];
+        gameCards = [...allCards.slice(0,10),
+                     ...allCards.slice(0,10)];
 
        
         gameBoard.style.gridTemplateColumns =
-            "repeat(6, 100px)";
+            "repeat(5,80px)";
     }
 }
 
@@ -178,28 +178,28 @@ function shuffleCards() {
 
 function createCards() {
 
-    // Clear old cards
+ 
     gameBoard.innerHTML = "";
 
-    // Create new cards
+ 
     gameCards.forEach((emoji) => {
 
-        // Create card element
+ 
         const card = document.createElement("div");
 
-        // Add class
+ 
         card.classList.add("card");
 
-        // Store emoji in dataset
+ 
         card.dataset.emoji = emoji;
 
-        // Hide emoji initially
+ 
         card.innerHTML = "?";
 
-        // Add click event
+ 
         card.addEventListener("click", flipCard);
 
-        // Add card to board
+ 
         gameBoard.appendChild(card);
 
     });
@@ -208,42 +208,42 @@ function createCards() {
 
 function flipCard() {
 
-    // Stop if game paused
+ 
     if (gamePaused) return;
 
-    // Stop if board locked
+ 
     if (lockBoard) return;
 
-    // Prevent clicking same card twice
+ 
     if (this === flippedCards[0]) return;
 
-    // Show emoji
+ 
     this.innerHTML = this.dataset.emoji;
 
-    // Add flipped style
+ 
     this.classList.add("flipped");
 
-    // Play sound
-// Reset sound
+ 
+ 
 if(!soundMuted){
 
     flipSound.currentTime = 0;
 
     flipSound.play();
 }
-    // Store card
+ 
     flippedCards.push(this);
 
-    // Check if 2 cards selected
+ 
     if (flippedCards.length === 2) {
 
-        // Increase moves
+ 
         moves++;
 
-        // Update UI
+ 
         movesDisplay.textContent = moves;
 
-        // Check match
+ 
         checkMatch();
     }
 }
@@ -271,12 +271,12 @@ function checkMatch() {
 
 function disableCards() {
 
-    // Remove click events
+ 
     flippedCards[0].removeEventListener("click", flipCard);
 
     flippedCards[1].removeEventListener("click", flipCard);
 
-    // Play match sound
+ 
 if(!soundMuted){
 
     matchSound.currentTime = 0;
@@ -286,41 +286,41 @@ if(!soundMuted){
    
     matchedCards += 2;
 
-    // Increase score
+ 
 score += 10 * combo;
 
-// Increase combo
+ 
 combo++;
 
-// Update score UI
+ 
 scoreDisplay.textContent = score;
-    // Check win
+ 
     checkWin();
 
-    // Reset board
+ 
     resetBoard();
 }
 
 
 function unflipCards() {
 
-    // Lock board
+ 
     lockBoard = true;
 
-    // Delay flip back
+ 
     setTimeout(() => {
 
-        // Hide emojis
+ 
         flippedCards[0].innerHTML = "?";
         flippedCards[1].innerHTML = "?";
 
-        // Remove flipped class
+ 
         flippedCards[0].classList.remove("flipped");
         flippedCards[1].classList.remove("flipped");
 
-        // Reset combo
+ 
 combo = 1;
-        // Reset board
+ 
         resetBoard();
 
     }, 1000);
@@ -339,10 +339,10 @@ function resetBoard() {
 
 function startTimer() {
 
-    // Prevent multiple timers
+ 
     clearInterval(timerInterval);
 
-    // Start timer
+ 
     timerInterval = setInterval(() => {
 
         timer++;
@@ -358,27 +358,27 @@ function checkWin() {
 
     if (matchedCards === gameCards.length) {
 
-        // Stop timer
+ 
         clearInterval(timerInterval);
 
-        // Save scores
+ 
         saveBestScore();
 
-        // Reload leaderboard
+ 
         loadBestScore();
 
-        // Stop background music
+ 
         bgMusic.pause();
 
-        // Play win sound
+ 
         winSound.currentTime = 0;
 
         winSound.play();
 
-        // Create confetti
+ 
         createConfetti();
 
-        // Show stats
+ 
         finalStats.innerHTML =
             `
             👤 ${currentPlayer}<br>
@@ -386,7 +386,7 @@ function checkWin() {
             ⏱️ Time: ${timer} sec
             `;
 
-            // Speed achievement
+ 
 if(timer <= 30){
 
     unlockAchievement(
@@ -394,7 +394,7 @@ if(timer <= 30){
     );
 }
 
-// Low move achievement
+ 
 if(moves <= 20){
 
     unlockAchievement(
@@ -402,7 +402,7 @@ if(moves <= 20){
     );
 }
 
-// Hard mode achievement
+ 
 if(currentDifficulty === "hard"){
 
     unlockAchievement(
@@ -410,7 +410,7 @@ if(currentDifficulty === "hard"){
     );
 }
 
-        // Show victory screen
+ 
         victoryScreen.classList.remove("hidden");
     }
 }
@@ -418,7 +418,7 @@ if(currentDifficulty === "hard"){
 
 function saveBestScore() {
 
-    // Unique keys per difficulty
+ 
     const movesKey =
         `bestMoves_${currentDifficulty}`;
 
@@ -428,11 +428,11 @@ function saveBestScore() {
     const timeKey =
         `bestTime_${currentDifficulty}`;
 
-    // Existing best score
+ 
     const bestMoves =
         localStorage.getItem(movesKey);
 
-    // Save only if better
+ 
     if(bestMoves === null || moves < bestMoves){
 
         localStorage.setItem(
@@ -455,11 +455,11 @@ function saveBestScore() {
 
 function loadBestScore() {
 
-    // Current selected difficulty
+ 
     const difficulty =
         difficultySelect.value;
 
-    // Dynamic keys
+ 
     const movesKey =
         `bestMoves_${difficulty}`;
 
@@ -469,7 +469,7 @@ function loadBestScore() {
     const timeKey =
         `bestTime_${difficulty}`;
 
-    // Load data
+ 
     const bestMoves =
         localStorage.getItem(movesKey);
 
@@ -479,7 +479,7 @@ function loadBestScore() {
     const bestTime =
         localStorage.getItem(timeKey);
 
-    // Update UI
+ 
     if(bestPlayer){
 
         bestPlayerDisplay.textContent =
@@ -510,34 +510,34 @@ function loadBestScore() {
 
 function createConfetti() {
 
-    // Create many particles
+ 
     for(let i = 0; i < 100; i++){
 
-        // Create confetti element
+ 
         const confetti =
             document.createElement("div");
 
-        // Add class
+ 
         confetti.classList.add("confetti");
 
-        // Random horizontal position
+ 
         confetti.style.left =
             Math.random() * 100 + "%";
 
-        // Random color
+ 
         confetti.style.backgroundColor =
             `hsl(${Math.random() * 360},
                  100%,
                  50%)`;
 
-        // Random animation duration
+ 
         confetti.style.animationDuration =
             Math.random() * 3 + 2 + "s";
 
-        // Add to container
+ 
         confettiContainer.appendChild(confetti);
 
-        // Remove later
+ 
         setTimeout(() => {
 
             confetti.remove();
@@ -549,20 +549,20 @@ function createConfetti() {
 
 function unlockAchievement(name) {
 
-    // Prevent duplicates
+ 
     if(achievements.includes(name)) return;
 
-    // Store achievement
+ 
     achievements.push(name);
 
-    // Create UI element
+ 
     const achievement =
         document.createElement("li");
 
     achievement.textContent =
         `🏆 ${name}`;
 
-    // Add to UI
+ 
     achievementList.appendChild(
         achievement
     );
@@ -572,10 +572,10 @@ function unlockAchievement(name) {
 
 function resetGameState(){
 
-    // Stop timer
+ 
     clearInterval(timerInterval);
 
-    // Reset variables
+ 
     moves = 0;
 
     timer = 0;
@@ -592,24 +592,24 @@ function resetGameState(){
 
     flippedCards = [];
 
-    // Clear achievements
+ 
     achievements = [];
 
     achievementList.innerHTML = "";
 
-    // Reset UI
+ 
     movesDisplay.textContent = 0;
 
     timerDisplay.textContent = 0;
 
     scoreDisplay.textContent = 0;
 
-    // Hide victory screen
+ 
     victoryScreen.classList.add(
         "hidden"
     );
 
-    // Clear board
+ 
     gameBoard.innerHTML = "";
 }
 
@@ -617,13 +617,13 @@ function resetGameState(){
 function startGame() {
 
  resetGameState();
-    // Get player name
+ 
 currentPlayer = playerNameInput.value.trim();
-// Store selected difficulty
+ 
 currentDifficulty =
     difficultySelect.value;
 
-// Prevent empty name
+ 
 if(currentPlayer === ""){
 
     alert("⚠ Please enter your name!");
@@ -632,19 +632,19 @@ if(currentPlayer === ""){
 }
     startScreen.style.display = "none";
 
-       // Play background music
+ 
 bgMusic.play();
 
-    // Set difficulty cards
+ 
     setDifficulty();
 
-    // Shuffle cards
+ 
     shuffleCards();
 
-    // Create cards
+ 
     createCards();
 
-    // Start timer
+ 
     startTimer();
 
     
@@ -654,22 +654,22 @@ bgMusic.play();
 
 function restartGame(){
 
-    // Reset everything
+ 
     resetGameState();
 
-    // Set difficulty again
+ 
     setDifficulty();
 
-    // Shuffle cards
+ 
     shuffleCards();
 
-    // Create cards
+ 
     createCards();
 
-    // Restart timer
+ 
     startTimer();
 
-    // Restart music
+ 
     bgMusic.currentTime = 0;
 
     if(!musicMuted){
@@ -681,15 +681,15 @@ function restartGame(){
 
 function exitGame(){
 
-    // Stop timer
+ 
     clearInterval(timerInterval);
 
-    // Stop music
+ 
     bgMusic.pause();
 
     bgMusic.currentTime = 0;
 
-    // Reset variables
+ 
     gamePaused = false;
 
     moves = 0;
@@ -704,41 +704,41 @@ function exitGame(){
 
     flippedCards = [];
 
-    // Reset displays
+ 
     movesDisplay.textContent = 0;
 
     timerDisplay.textContent = 0;
 
     scoreDisplay.textContent = 0;
 
-    // Clear cards
+ 
     gameBoard.innerHTML = "";
 
-    // Hide victory screen
+ 
     victoryScreen.classList.add(
         "hidden"
     );
 
-    // SHOW START SCREEN
+ 
     startScreen.classList.remove(
         "hidden"
     );
 
-    // Make sure it displays correctly
+ 
     startScreen.style.display = "flex";
 }
 
 
 function togglePause() {
 
-    // Toggle state
+ 
     gamePaused = !gamePaused;
 
     if (gamePaused) {
 
     clearInterval(timerInterval);
 
-    // Pause music
+ 
     bgMusic.pause();
 
     pauseButton.textContent = "Resume";
@@ -747,7 +747,7 @@ function togglePause() {
 
     startTimer();
 
-    // Resume music
+ 
     bgMusic.play();
 
     pauseButton.textContent = "Pause";
@@ -757,19 +757,19 @@ function togglePause() {
 
 function nextLevel(){
 
-    // Easy -> Medium
+ 
     if(currentDifficulty === "easy"){
 
         difficultySelect.value = "medium";
     }
 
-    // Medium -> Hard
+ 
     else if(currentDifficulty === "medium"){
 
         difficultySelect.value = "hard";
     }
 
-    // Hard completed
+ 
     else{
 
         alert("🏆 All levels completed!");
@@ -777,11 +777,11 @@ function nextLevel(){
         return;
     }
 
-    // Update current difficulty
+ 
     currentDifficulty =
         difficultySelect.value;
 
-    // Restart game
+ 
     restartGame();
 }
 
@@ -794,20 +794,20 @@ function closeVictoryScreen(){
     );
 }
 
-// EVENT LISTENERS
+ 
 
-// Start game
+ 
 startButton.addEventListener("click", startGame);
 
-// Restart game
+ 
 restartButton.addEventListener("click", restartGame);
 
-// Pause game
+ 
 pauseButton.addEventListener("click", togglePause);
-// Load saved leaderboard
+ 
 loadBestScore();
-// Play again button
-// Play again
+ 
+ 
 playAgainButton.addEventListener(
     "click",
     () => {
@@ -815,24 +815,24 @@ playAgainButton.addEventListener(
         restartGame();
     }
 );
-// Update leaderboard
+ 
 difficultySelect.addEventListener(
     "change",
     loadBestScore
 );
-// Next level
+ 
 nextLevelButton.addEventListener(
     "click",
     nextLevel
 );
 
 
-// Close victory screen
+ 
 closeVictoryButton.addEventListener(
     "click",
     closeVictoryScreen
 );
-// Exit game
+ 
 exitButton.addEventListener(
     "click",
     exitGame
